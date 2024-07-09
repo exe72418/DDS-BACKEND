abf0e85414e54f8616a50a160069e18e63018691
BrunoMar99 <brunoemartinez99@gmail.com>
2024-07-09 19:57:01 -0300

    CRUD ENTREGA TERMINADO

--- src/controllers/entregaController.ts
+++ src/controllers/entregaController.ts
@@ -16,7 +16,15 @@ async function findAll(req: Request, res: Response) {
 }
 
 async function findOne(req: Request, res: Response) {
-
+  try {
+    const id = Number.parseInt(req.params.id)
+    const EntregaEncontrada = await em.findOneOrFail(Entrega, { id },{populate:['repartidor']})
+    res
+      .status(200)
+      .json({ message: 'se encontro la entrega!', data: EntregaEncontrada })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 async function add(req: Request, res: Response) {
@@ -34,11 +42,26 @@ async function add(req: Request, res: Response) {
 }
 
 async function update(req: Request, res: Response) {
-
+  try {
+    const id = Number.parseInt(req.params.id)
+    const entregaToUpdate = await em.findOneOrFail(Entrega, {id})
+    em.assign(entregaToUpdate, req.body)
+    await em.flush()
+    res.status(200).json({ message: 'Entrega actualizada!', data: entregaToUpdate })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 async function remove(req: Request, res: Response) {
-
+  try {
+    const id = Number.parseInt(req.params.id)
+    const entrega = await em.getReference(Entrega, id)
+    await em.removeAndFlush(entrega)
+    res.status(200).json({ message: 'Entrega borrada!' })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 export { findAll, findOne, add, update, remove }
